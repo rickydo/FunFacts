e87f1fc2d3eb69b1d13667ec7fd9572ca3cbd798
rickydo <g3tdo@yahoo.com>
2015-04-14 13:00:52 -0500

    adding variables for button and text views

--- app/src/main/java/com/example/admin/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/admin/funfacts/FunFactsActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,6 +14,10 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare our view variables and assign view from layout file
+        TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
     }
 
 
